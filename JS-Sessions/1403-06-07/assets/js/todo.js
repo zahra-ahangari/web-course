875aa6a2e3003bba5afeb3646725105186fc5f90
Zahra Ahangari <ahangari95@gmail.com>
2024-09-11 16:14:46 +0330

    Append updated html content

--- JS-Sessions/1403-06-07/assets/js/todo.js
+++ JS-Sessions/1403-06-07/assets/js/todo.js
@@ -17,6 +17,7 @@ function todoGenerator(itemValue) {
     todoItems.push(valueObj);
     console.log("Newly added item: ", valueObj);
     console.log("Updated list: ", todoItems);
+    generateListForUI(valueObj);
 }
 
 $(".auth-login").submit((e) => {
@@ -25,15 +26,28 @@ $(".auth-login").submit((e) => {
     todoInput.val("");  
 });
 
-function generateListForUI(items){
-    console.log("Final list is: ", items);
-    items.map((item, index) => {
-        result = `<li id='${item.id}' class="d-flex gap-2 justify-content-between">
-                <span>Task 1</span>
-                <div class="todos-actions d-flex gap-2">
-                    <button class="btn btn-primary">Edit</button>
-                    <button class="btn btn-danger">Delete</button>
-                </div>
-            </li>`
-    })
+function generateListForUI(item){
+    // console.log("Final list is: ", items);
+    // var newHtml = items.map((item, index) => {
+    //     result = 
+    //     `<li id='${item.id}' class="d-flex gap-2 justify-content-between">
+    //         <span>${item.title}</span>
+    //         <div class="todos-actions d-flex gap-2">
+    //             <button class="btn btn-primary">Edit</button>
+    //             <button class="btn btn-danger">Delete</button>
+    //         </div>
+    //     </li>`
+    //     return result;
+    // })
+
+    var newHtml = 
+        `<li id='${item.id}' class="d-flex gap-2 justify-content-between">
+            <span>${item.title}</span>
+            <div class="todos-actions d-flex gap-2">
+                <button class="btn btn-primary">Edit</button>
+                <button class="btn btn-danger">Delete</button>
+            </div>
+        </li>`
+
+    $('#content').append(newHtml);
 }
